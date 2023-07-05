8b5b56d192ed425b46fc0e3f52c9bfaa9e5fc577
Anjali Chitkara <55926234+anjali-chitkara@users.noreply.github.com>
2023-07-05 14:30:50 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,10 +1,15 @@
-// Intercept and dismiss the WebView, and open the tearsheet link
+// Intercept, dismiss the WebView, and open the tearsheet
 window.addEventListener('DOMContentLoaded', function() {
   var aaplLink = document.querySelector('a[href="sofimobile://wealth/brokerage_tearsheet/AAPL"]');
   if (aaplLink) {
     aaplLink.addEventListener('click', function(event) {
       event.preventDefault();
-      window.location.href = 'sofimobile://wealth/brokerage_tearsheet/AAPL';
+      // Dismiss the WebView
+      window.location.href = 'sofimobile://dismiss';
+      // Open the tearsheet
+      setTimeout(function() {
+        window.location.href = 'sofimobile://wealth/brokerage_tearsheet/AAPL';
+      }, 500); // Adjust the timeout delay as needed
     });
   }
 });
